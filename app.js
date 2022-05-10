399e4beae7d28419b363137ac90d9ebe7a8f23b5
Hasmik Ananyan <hasmik@ananyan.com>
2022-05-10 17:18:31 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+import userRoutes from "./routes/user.route.js";
+import connect from "./connection.js";
+import config from "./config/config.service.js";
+import categoryRoute from "./routes/category.route.js";
+import subcategoryRoute from "./routes/subcategory.route.js";
+import courseRoute from "./routes/course.route.js";
+
+const app = express();
+
+connect.connect;
+app.use(express.json());
+
+app.use(userRoutes);
+app.use(categoryRoute);
+app.use(subcategoryRoute);
+app.use(courseRoute);
+
+if (config.isValid()) {
+  process.exit(0);
+}
+
+app.listen(process.env.PORT);
